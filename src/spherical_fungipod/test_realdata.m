7130f980732f7562a7c2972fe39e02b643e4975a
Tian Cao <tiancao@cs.unc.edu>
2012-02-24 20:42:43 -0500

    test with 0,1 vs. others

--- src/spherical_fungipod/test_realdata.m
+++ src/spherical_fungipod/test_realdata.m
@@ -1,7 +1,8 @@
 clear all
+clc
 
-
-folder = [];
+load('descriptor_5.mat');
+%folder = [];
 
 no_of_img = 121*ones(1,30);
 
@@ -43,75 +44,21 @@ grouplabel = [3 0 1(2) ...
     4 0 0 4 2];
 %}
 
-%{
-grouplabel = [3 0 1 ...
-    1 1 1 2 ...
-    1 ...
-    1 2 1 1 ...
-    2 2 ...
-    1 1 4 1 0 1 ...
-    1 3 1 4 2 ...
-    4 0 0 4 2];
-%}
 % 1 and VS others
 grouplabel = [1 0 0 ...
-    0 0 0 1 ...
+    1 0 1 1 ...
     0 ...
     0 1 0 0 ...
     1 1 ...
     0 0 1 0 0 0 ...
-    0 1 0 1 1 ...
-    1 0 0 1 1];
+    0 0 0 1 1 ...
+    1 0 1 1 1];
 
 %center = [center grouplabel'];
 %center = sortrows(center, 5);
 %[folder, center, no_of_img] = folders_and_yeast_centers();
 %folder = [folder ; 'G:/Users/serdar/Desktop/pseudo/perfectsphere'];
 
-% each one is a folder that holds 2D stack of images of a yeast-fungipod
-% complex (i.e. not the whole specimen with many yeasts, but just a single
-% yeast with its fungipods)
-%folder = [folder ; 'C:/Users/cakici/Desktop/test_image_maker/g000'];
-
-%%{
-folder = [folder ; '../data/fungipod numbers/clipped_actin/1/1'];
-folder = [folder ; '../data/fungipod numbers/clipped_actin/1/2'];
-folder = [folder ; '../data/fungipod numbers/clipped_actin/1/3'];
-
-folder = [folder ; '../data/fungipod numbers/clipped_actin/2/1'];
-folder = [folder ; '../data/fungipod numbers/clipped_actin/2/2'];
-folder = [folder ; '../data/fungipod numbers/clipped_actin/2/3'];
-folder = [folder ; '../data/fungipod numbers/clipped_actin/2/4'];
-
-folder = [folder ; '../data/fungipod numbers/clipped_actin/3/1'];
-
-folder = [folder ; '../data/fungipod numbers/clipped_actin/4/1'];
-folder = [folder ; '../data/fungipod numbers/clipped_actin/4/2'];
-folder = [folder ; '../data/fungipod numbers/clipped_actin/4/3'];
-folder = [folder ; '../data/fungipod numbers/clipped_actin/4/4'];
-
-folder = [folder ; '../data/fungipod numbers/clipped_actin/5/1'];
-folder = [folder ; '../data/fungipod numbers/clipped_actin/5/2'];
-
-folder = [folder ; '../data/fungipod numbers/clipped_actin/6/1'];
-folder = [folder ; '../data/fungipod numbers/clipped_actin/6/2'];
-folder = [folder ; '../data/fungipod numbers/clipped_actin/6/3'];
-folder = [folder ; '../data/fungipod numbers/clipped_actin/6/4'];
-folder = [folder ; '../data/fungipod numbers/clipped_actin/6/5'];
-folder = [folder ; '../data/fungipod numbers/clipped_actin/6/6'];
-
-folder = [folder ; '../data/fungipod numbers/clipped_actin/7/1'];
-folder = [folder ; '../data/fungipod numbers/clipped_actin/7/2'];
-folder = [folder ; '../data/fungipod numbers/clipped_actin/7/3'];
-folder = [folder ; '../data/fungipod numbers/clipped_actin/7/4'];
-folder = [folder ; '../data/fungipod numbers/clipped_actin/7/5'];
-
-folder = [folder ; '../data/fungipod numbers/clipped_actin/8/1'];
-folder = [folder ; '../data/fungipod numbers/clipped_actin/8/2'];
-folder = [folder ; '../data/fungipod numbers/clipped_actin/8/3'];
-folder = [folder ; '../data/fungipod numbers/clipped_actin/8/4'];
-folder = [folder ; '../data/fungipod numbers/clipped_actin/8/5'];
-%%}
 
 %{
 voxeldim = 0.414;
@@ -127,6 +74,7 @@ classrate = zeros(10,10);
 
 for inoradii = 1:10
     for iradiiyeast = 1:10
+        %{
         no_of_radii = inoradii;
         radius_of_yeast = iradiiyeast*voxeldim;
         %%}
@@ -160,17 +108,15 @@ for inoradii = 1:10
         all = invariant;
         %all = cat(3,invariant_with_fung_1,invariant_with_fung_2,invariant_with_fung_3);
         conf_all = squeeze(all)';
-        all_dist= dist2(conf_all,conf_all);
+        %}
+        
+        %% use descriptor.mat
+        conf_all = descriptor{iradiiyeast + (inoradii-1)*10};
+        %%
+        
+        %all_dist= dist2(conf_all,conf_all);
         %figure, imagesc(all_dist), colorbar, colormap hot, title ''
-        %%%%%%%%%%%%
-        
-        %temp_fung = squeeze(all_invariant)';
-        %burdaki ' olayina dikkat. conjugate aliyo gene galiba. disa bi tane "conj"
-        %koyman gerekebilir.
-        
-        %with_1_fung = temp_fung(1:4,:);
-        %with_2_fung = temp_fung(5:8,:);
-        %with_3_fung = temp_fung(9:12,:);
+        %%
         
         %%
         %all_dist= dist2(temp_fung,temp_fung);
@@ -199,9 +145,9 @@ for inoradii = 1:10
             group = labelclean;
             group(i,:) = [];
             
-            label(i) = knnclassify(test, training, group);
+            label(i) = knnclassify(test, training, group, 3);
             
-            %display([num2str(i) 'th test case labeled as ' num2str(label(i)) ', true label is ' num2str(labelclean(i))]);
+            % display([num2str(i) 'th test case labeled as ' num2str(label(i)) ', true label is ' num2str(labelclean(i))]);
         end
         
         classrate(inoradii, iradiiyeast) = sum((label' - labelclean)==0)/size(labelclean,1);
@@ -210,5 +156,5 @@ for inoradii = 1:10
         
     end
 end
-
+max(classrate(:))
 %classrate
